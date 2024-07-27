8cb9ad4ce4f060bf8ecb0300dc55dc293ee85d16
Haylas <104948588+etherbiln@users.noreply.github.com>
2024-07-27 19:25:21 +0300

    restore
    restore

--- contracts/MockERC721.sol
+++ contracts/MockERC721.sol
@@ -1,14 +1,13 @@
 // contracts/MockERC721.sol
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MockERC721 is ERC721 {
-    uint256 private _tokenId;
+    uint256 private _tokenIds;
 
-    constructor() ERC721("MockERC721", "M721") {}
+    constructor() ERC721("MockERC721", "MCK") {}
 
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
---
+++ scripts/Deploy.js
@@ -0,0 +1,28 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const balance = await deployer.getBalance();
+    console.log("Account balance:", balance.toString());
+  
+    const NFTMarketplace = await ethers.getContractFactory("NFTMarketplace");
+    const nftMarketplace = await NFTMarketplace.deploy();
+    await nftMarketplace.deployed();
+  
+    console.log("NFTMarketplace deployed to:", nftMarketplace.address);
+  
+    const MockERC721 = await ethers.getContractFactory("MockERC721");
+    const mockERC721 = await MockERC721.deploy();
+    await mockERC721.deployed();
+  
+    console.log("MockERC721 deployed to:", mockERC721.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
