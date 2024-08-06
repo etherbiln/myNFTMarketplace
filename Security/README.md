bcf5e4e603963944405a231ac548e003e9690a67
Haylas <104948588+etherbiln@users.noreply.github.com>
2024-08-06 20:22:44 +0300

    Restore-Reentrancy

--- Security/README.md
+++ Security/README.md
@@ -7,5 +7,4 @@ This project has undergone security testing and improvements using Slither and E
 - **Slither**: A static analysis tool for Solidity smart contracts. It helps improve code quality and detect security vulnerabilities.
 - **Echidna**: A property-based testing tool for smart contracts. It performs randomized tests to uncover potential bugs and security issues.
 
-![Slither](images/Slither.png)
-
+![Slither](./images/slither.png)
--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,33 +1,35 @@
-// contracts/NFTMarketplace.sol
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
-contract NFTMarketplace {
+contract NFTMarketplace is Initializable {
     address public owner;
-    uint idForSale;
-    uint idForAuction;
-    uint  price = msg.value * 95 / 100;
+    uint256 idForSale;
+    uint256 idForAuction;
+    uint256  price = msg.value * 95 / 100;
 
     struct ItemForSale {
         address contractAddress;
         address seller;
         address buyer;
+        address creator; 
         uint price;
         uint tokenId;
+        uint royaltyPercentage;
         bool state;
-        bool isSold;
     }
 
     struct ItemForAuction {
         address contractAddress;
         address seller;
         address buyer;
+        address creator;
         uint startingPrice;
         uint highestBid;
         uint tokenId;
+        uint royaltyPercentage; 
         uint deadline;
         bool state;
         bool isFinished;
@@ -36,64 +38,70 @@ contract NFTMarketplace {
     mapping(uint => ItemForSale) public idToItemForSale;
     mapping(uint => ItemForAuction) public idToItemForAuction;
 
-    constructor() {
+    function initialize() public initializer {
         owner = msg.sender;
     }
 
-    function startNFTSale(address contractAddress, uint priceNFT, uint tokenId) public {
+    function startNFTSale(address contractAddress, uint256 priceNFT, uint256 tokenId, uint256 royaltyPercentage) public {
         IERC721 NFT = IERC721(contractAddress);
         require(NFT.ownerOf(tokenId) == msg.sender, "You are not owner of this NFT!");
         NFT.transferFrom(msg.sender, address(this), tokenId);
-        idToItemForSale[idForSale] = ItemForSale(contractAddress, msg.sender, msg.sender, priceNFT, tokenId, false,false);
+        idToItemForSale[idForSale] = ItemForSale(contractAddress, msg.sender, msg.sender, msg.sender, priceNFT, tokenId, royaltyPercentage,false);
         idForSale += 1;
     }
 
-    function cancelNFTSale(uint Id) public {
-        ItemForSale memory info = idToItemForSale[Id];
-        IERC721 NFT = IERC721(info.contractAddress);
-        require(info.seller == msg.sender, "You are not owner of this NFT!");
-        require(info.state == false, "This NFT sold!");
-        NFT.transferFrom(address(this), msg.sender, info.tokenId);
-        idToItemForSale[Id] = ItemForSale(address(0), address(0), address(0), 0, 0, true,false);
-    }
-
-    function startNFTAuction(address contractAddress, uint priceNFT, uint tokenId, uint deadline) public {
+    function startNFTAuction(address contractAddress, uint256 priceNFT, uint256 tokenId, uint256 deadline, uint256 royaltyPercentage) public {
         IERC721 NFT = IERC721(contractAddress);
         require(NFT.ownerOf(tokenId) == msg.sender, "You are not owner of this NFT!");
+        
         NFT.transferFrom(msg.sender, address(this), tokenId);
-        idToItemForAuction[idForAuction] = ItemForAuction(contractAddress, msg.sender, msg.sender, priceNFT, 0, tokenId, deadline, false,false);
+        idToItemForAuction[idForAuction] = ItemForAuction(contractAddress, msg.sender, msg.sender, msg.sender, priceNFT, 0, tokenId, royaltyPercentage, deadline, false, false);
         idForAuction += 1;
     }
 
-    function cancelNFTAuction(uint Id) public {
-        ItemForAuction memory info = idToItemForAuction[Id];
-        IERC721 NFT = IERC721(info.contractAddress);
+    function cancelNFTAuction(uint256 Id) public {
+        ItemForAuction storage info = idToItemForAuction[Id];
         require(info.seller == msg.sender, "You are not owner of this NFT!");
         require(info.state == false, "This NFT sold!");
+    
+        info.state = true;
+    
+        IERC721 NFT = IERC721(info.contractAddress);
         NFT.transferFrom(address(this), msg.sender, info.tokenId);
-        idToItemForAuction[Id] = ItemForAuction(address(0), address(0), address(0), 0, 0, 0, 0, true,false);
     }
 
-    function buyNFT(uint Id) payable public {
+    function cancelNFTSale(uint256 Id) public {
+        ItemForSale storage info = idToItemForSale[Id];
+        require(info.seller == msg.sender, "You are not owner of this NFT!");
+        require(info.state == false, "This NFT sold!");
+    
+        info.state = true;
+    
+        IERC721 NFT = IERC721(info.contractAddress);
+        NFT.transferFrom(address(this), msg.sender, info.tokenId);
+    }
+
+    function buyNFT(uint256 Id) public payable {
         ItemForSale storage info = idToItemForSale[Id];
         require(Id < idForSale, "Invalid sale ID");
         require(msg.sender != info.seller, "You are the seller");
         require(msg.value == info.price, "Incorrect price");
-        require(!info.isSold, "NFT already sold");
+        require(!info.state, "NFT already sold");
 
-        // Update state before external calls
         info.buyer = msg.sender;
-        info.isSold = true;
+        info.state = true;
 
         IERC721 NFT = IERC721(info.contractAddress);
         NFT.transferFrom(address(this), msg.sender, info.tokenId);
 
-        uint sellerAmount = msg.value * 95 / 100;
+        uint royaltyAmount = msg.value * info.royaltyPercentage / 100;
+        uint sellerAmount = msg.value * 95 / 100 - royaltyAmount;
+        payable(info.creator).transfer(royaltyAmount);
         payable(info.seller).transfer(sellerAmount);
-        payable(owner).transfer(msg.value - sellerAmount);
+        payable(owner).transfer(msg.value - sellerAmount - royaltyAmount);
     }
 
-    function bid(uint Id) payable public {
+    function bid(uint256 Id) public payable {
         ItemForAuction storage info = idToItemForAuction[Id];
         require(Id < idForAuction);
         require(msg.sender != info.seller, "You are seller");
@@ -110,8 +118,11 @@ contract NFTMarketplace {
             info.buyer = msg.sender;
             info.highestBid = msg.value;
         }
+        info.buyer = msg.sender;
+        info.state = true;
     }
-    function finishNFTAuction(uint Id) public {
+
+    function finishNFTAuction(uint256 Id) public {
         ItemForAuction storage info = idToItemForAuction[Id];
         require(Id < idForAuction, "Invalid auction ID");
         require(msg.sender == info.buyer, "You are not the highest bidder");
@@ -119,15 +130,15 @@ contract NFTMarketplace {
         require(block.timestamp > info.deadline, "Auction not yet finished");
         require(info.buyer != info.seller, "No bids received");
 
-        // Update state before external calls
         info.isFinished = true;
 
         IERC721 NFT = IERC721(info.contractAddress);
         NFT.transferFrom(address(this), msg.sender, info.tokenId);
 
-        uint sellerAmount = info.highestBid * 95 / 100;
+        uint256 royaltyAmount = info.highestBid * info.royaltyPercentage / 100;
+        uint256 sellerAmount = info.highestBid * 95 / 100 - royaltyAmount;
+        payable(info.creator).transfer(royaltyAmount);
         payable(info.seller).transfer(sellerAmount);
-        payable(owner).transfer(info.highestBid - sellerAmount);
+        payable(owner).transfer(info.highestBid - sellerAmount - royaltyAmount);
     }
-
 }
