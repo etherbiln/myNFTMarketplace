989354c77e78cce82d9d03c0495b0c1f9ffb8ac0
Haylas <104948588+etherbiln@users.noreply.github.com>
2024-08-07 11:38:22 +0300

    Uptade Deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-require('@nomicfoundation/hardhat-toolbox');
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
@@ -9,7 +10,23 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY],
     },
     hardhat: {
-      chainId: 1337
+      chainId: 1337,
     },
   },
 };
+
+// require('dotenv').config();
+// require('@nomicfoundation/hardhat-toolbox');
+
+// module.exports = {
+//   solidity: "0.8.20",
+//   networks: {
+//     sepolia: {
+//       url: process.env.INFURA_SEPOLIA_URL,
+//       accounts: [process.env.PRIVATE_KEY],
+//     },
+//     hardhat: {
+//       chainId: 1337
+//     },
+//   },
+// };
